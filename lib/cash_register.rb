ae82f42c4aaa360ece58e371c172cdd9a670685c
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-02-25 21:01:27 -0500

    commit tree 79efdbffb621bcc4e76f00b094a499f32489061d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,38 +1,71 @@
 # include 'pry'
+# class CashRegister
+
+#     attr_accessor :total, :discount, :item
+#     @@all_items = []
+#     @@all_prices = []
+#     def initialize(discount = 20)
+#         @total = 0
+#         @discount = discount
+#     end
+
+#     def add_item(title, price, quantity = 1)
+#         @total += price * quantity
+#         @@all_items.push(title)
+#         @@all_prices.push(price)
+#     end
+
+#     def apply_discount 
+#         @total -= (discount * (total / 100))
+#         # binding.pry
+#         "After the discount, the total comes to $#{total}."
+#     end
+
+#     def item
+#         @@all_items
+#     end
+
+#     def void_last_transaction
+#         lastItem = @@all_prices.pop()
+#         @total -= lastItem
+#     end
+# end
+
+
+
+
+
 class CashRegister
 
-    attr_accessor :total, :discount, :item
-    @@all_items = []
-    @@all_prices = []
-    def initialize(total = 0, discount = 20)
-        @total = total
-        @discount = discount
+    attr_accessor :items, :discount, :total, :last_transaction
+  
+    def initialize(discount=0)
+      @total = 0
+      @discount = discount
+      @items = []
     end
-
-    def add_item(title, price, quantity = 1)
-        @total += price * quantity
-        @@all_items.push(title)
-        @@all_prices.push(price)
+  
+    def add_item(title, amount, quantity=1)
+      self.total += amount * quantity
+      quantity.times do
+        items << title
+      end
+      @last_transaction = amount * quantity
     end
-
-    def apply_discount 
-        @total -= (discount * (total / 100))
-        # binding.pry
-        "After the discount, the total comes to $#{total}."
+  
+    def apply_discount
+      if discount != 0
+        self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+        "After the discount, the total comes to $#{self.total}."
+      else
+        "There is no discount to apply."
+      end
     end
-
-    def item
-        @@all_items
-    end
-
+  
     def void_last_transaction
-        lastItem = @@all_prices.pop()
-        @total -= lastItem
-        
+      self.total = self.total - self.last_transaction
     end
-
-
-end
+  end
 
 cash = CashRegister.new
 cash.add_item("macbook air", 1000)
